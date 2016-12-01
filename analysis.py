5b6505838fd82a59862622e4440ad7e7ef4d1977
Christopher Syers <csyers@nd.edu>
2016-12-01 15:22:45 -0500

    errorbar

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
 from numpy import array
+import numpy as np
+
 with open("V1432_AQL_TC_V2.txt") as f:
     reader = csv.reader(f)
     headers = reader.next()
@@ -10,9 +12,18 @@ with open("V1432_AQL_TC_V2.txt") as f:
     for row in reader:
         for h, v in zip(headers,row):
             data[h].append(v)
-    
-    magnitudes = array(data["MAG"])
 
-    plt.plot(magnitudes)
+    # magnitudes - numpy array of all magnitude measurements 
+    magnitudes = array([float(i) for i in data["MAG"]])
+
+    # xvals - numpy array 1-792 for the x values of the measurements
+    xvals = array(list(range(1,len(magnitudes)+1)))
+
+    # yerrs - numpy array of the measured magnitude errors
+    yerrs = array([float(i) for i in data["MAGERR"]])
+
+
+    plt.figure()
+    plt.errorbar(xvals, magnitudes, yerr= yerrs)
     plt.gca().invert_yaxis()
     plt.show()
